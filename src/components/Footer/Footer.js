5ee7713852aa8f9dccec6b016e24e0750b967379
Mikael Sahleström <mikael.sahlestrom@prevas.se>
2019-11-14 11:02:04 +0100

    add img and description

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import classes from './Footer.module.css';
 const Footer = () => {
     return (
         <div className={classes.Footer}>
-            This is my bottom place
+            This is my bottom bitch
         </div>
     );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import classes from './Header.module.css';
 
+import Description from './Description/Description'
+import myself from '../../assets/myself.svg'
+
 const Header = () => {
     return (
         <div className={classes.Header}>
-            This is my stupid header      
+            <img src={myself} className={classes.MyImg} alt="logo" />
+            <Description />    
         </div>
     );
 };
