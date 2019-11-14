89845e9df670d5ebfd4f4e112a711cdcad3f1a0a
Mikael Sahleström <mikael.sahlestrom@prevas.se>
2019-11-14 11:23:16 +0100

    initial style for header

--- src/components/Header/Description/Description.js
+++ src/components/Header/Description/Description.js
@@ -1,13 +1,42 @@
-import React from 'react';
-import classes from './Description.module.css';
+import React from "react";
+import classes from "./Description.module.css";
 
 const Description = () => {
-    return (
-        <div className={classes.Description}>
-            General description of myself
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-    );
+  return (
+    <div className={classes.Description}>
+      <h1>General description of myself</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Et leo duis ut diam
+        quam nulla porttitor massa id. Tellus in metus vulputate eu scelerisque
+        felis imperdiet proin fermentum. Aenean pharetra magna ac placerat
+        vestibulum lectus. Sagittis purus sit amet volutpat consequat. Fermentum
+        dui faucibus in ornare quam viverra orci sagittis eu. Sem nulla pharetra
+        diam sit amet nisl suscipit adipiscing bibendum. Felis eget nunc
+        lobortis mattis aliquam faucibus purus in. Quam adipiscing vitae proin
+        sagittis nisl. Mi proin sed libero enim sed faucibus turpis. Varius vel
+        pharetra vel turpis nunc. Urna molestie at elementum eu facilisis sed
+        odio. Justo donec enim diam vulputate ut pharetra sit amet aliquam.
+        Sodales ut etiam sit amet nisl purus in. Nisi vitae suscipit tellus
+        mauris a diam. Ullamcorper dignissim cras tincidunt lobortis. Turpis
+        massa tincidunt dui ut ornare lectus sit amet. Pulvinar pellentesque
+        habitant morbi tristique senectus et netus et malesuada. Sem viverra
+        aliquet eget sit. At ultrices mi tempus imperdiet nulla malesuada
+        pellentesque elit eget. Massa sapien faucibus et molestie ac feugiat
+        sed. Tincidunt lobortis feugiat vivamus at augue eget arcu dictum
+        varius. Diam in arcu cursus euismod quis viverra nibh cras pulvinar.
+        Varius sit amet mattis vulputate enim nulla aliquet. Imperdiet nulla
+        malesuada pellentesque elit eget gravida cum. Nec ultrices dui sapien
+        eget mi. Adipiscing commodo elit at imperdiet dui accumsan. Nunc
+        pulvinar sapien et ligula ullamcorper malesuada proin libero. Non
+        curabitur gravida arcu ac. Cursus turpis massa tincidunt dui ut ornare
+        lectus. Massa sed elementum tempus egestas. Cursus in hac habitasse
+        platea. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras
+        fermentum. Eu tincidunt tortor aliquam nulla facilisi cras. Et malesuada
+        fames ac turpis egestas.
+      </p>
+    </div>
+  );
 };
 
 export default Description;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ import myself from '../../assets/myself.svg'
 
 const Header = () => {
     return (
-        <div className={classes.Header}>
+        <div className={classes.HeaderContainer}>
             <img src={myself} className={classes.MyImg} alt="logo" />
             <Description />    
         </div>
